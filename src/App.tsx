1571b51c17438b918ac558923b37c2a22077cb4b
Valtteri Tastula <tastula@protonmail.com>
2021-01-13 21:43:36 +0200

    Working implementation of prerequisite search

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import someCourses from './res/someCourses.json';
-import { CourseUnit } from './models';
+import tieCourses from './res/tieCourses.json';
+import { CourseUnit, PrerequisiteGroup } from './models';
 
-const courseUnits: CourseUnit = someCourses;
+// Make courses available by both internal groupId and known course code
+const indexedCourses = new Map<string, CourseUnit>();
+tieCourses.forEach((course) => {
+  indexedCourses.set(course.groupId, course);
+  indexedCourses.set(course.code, course);
+});
+
+interface CourseTreeNode {
+  id: string;
+  text_1: string;
+  father: string | null;
+}
+
+const nodes: CourseTreeNode[] = [];
+
+const createNodesForCourse = (preGroup: PrerequisiteGroup, preGroupId: string) => {
+  preGroup.prerequisites.forEach((prerequisite) => {
+    // TODO: Support also study modules
+    if(prerequisite?.courseUnitGroupId) {
+      const course = indexedCourses.get(prerequisite.courseUnitGroupId);
+      if(course) {
+        nodes.push({
+          id: course.code,
+          text_1: course.name.fi ?? course.name.en ?? 'error',
+          father: preGroupId,
+        });
+        createNodesForPreGroup(course);
+      }
+    }
+  })
+};
+
+const createNodesForPreGroup = (course: CourseUnit) => {
+  course.compulsoryFormalPrerequisites?.forEach((preGroup, idx) => {
+    if(preGroup.prerequisites.length !== 0) {
+      const preGroupId = `${course.code}preGroup${idx + 1}`;
+      nodes.push({
+        id: preGroupId,
+        text_1: `Group ${idx}`,
+        father: course.code,
+      });
+      createNodesForCourse(preGroup, preGroupId);
+    }
+  });
+};
+
+const createNodes = (courseCode: string) => {
+  const course = indexedCourses.get(courseCode);
+  if(course) {
+    nodes.push({
+      id: course.code,
+      text_1: course.name.fi ?? course.name.en ?? 'error',
+      father: null,
+    });
+    createNodesForPreGroup(course);
+  }
+};
+
+createNodes("COMP.CS.140");
+console.log(nodes);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {tieCourses.length}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {courseUnits.length}
-        </a>
       </header>
     </div>
   );
